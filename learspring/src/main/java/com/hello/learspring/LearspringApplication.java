dd6e7e40c20124805c81ed0955b87168754ba20d
blackjune67 <buea486@gmail.com>
2024-05-09 12:38:20 +0900

    spring basic learn

---
+++ learspring/src/main/java/com/hello/learspring/LearspringApplication.java
@@ -0,0 +1,13 @@
+package com.hello.learspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearspringApplication.class, args);
+	}
+
+}
---
+++ learspring/src/test/java/com/hello/learspring/LearspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello.learspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
