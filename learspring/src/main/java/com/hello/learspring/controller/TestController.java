fa8f27575f0ec1533caedfbe749792d62ce39a32
blackjune67 <buea486@gmail.com>
2024-05-09 13:06:12 +0900

    test commit001

--- learspring/src/main/java/com/hello/learspring/controller/TestController.java
+++ learspring/src/main/java/com/hello/learspring/controller/TestController.java
@@ -1,10 +1,14 @@
 package com.hello.learspring.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
 
-
-    public void test() {
-        System.out.println("Hello, TestController");
+    @GetMapping("/test")
+    public String test() {
+        return "안녕하세요!!!";
     }
 }
 
